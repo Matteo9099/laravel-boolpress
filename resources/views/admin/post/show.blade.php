c67b5b4440f1bc0aab82b906d4884be1c1063c89
Matteo Morganti <matteo99morganti@gmail.com>
2022-04-13 15:46:31 +0200

    first commit

---
+++ resources/views/admin/post/show.blade.php
@@ -0,0 +1,27 @@
+@extends('admin.layouts.base')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+
+                <h1>Visualizza Post</h1>
+
+                <div><strong>Titolo: </strong>{{$post->title}}</div>
+                <div><strong>Contenuto: </strong>{!! $post->content !!}</div>
+                <div><strong>Slug: </strong>{{$post->slug}}</div>
+                <div><strong>Categoria: </strong>{{$post->category->name}}</div>
+                <div><strong>Post scritto: </strong>{{$diffInDays}} giorni fa</div>
+                
+                <div>
+                    @foreach ($post->tags as $tag)
+                        <span class="badge badge-primary">{{$tag->name}}</span>
+                    @endforeach
+                </div>
+
+                <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna alla lista</a>
+
+            </div>
+        </div>
+    @endsection
+
